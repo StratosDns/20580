cf926073c30f77335acb94259959dec6b2b1ca30
StratosDns <th20580@edu.hmu.gr>
2024-03-19 20:47:36 +0200

    git commit

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -9,6 +9,48 @@
 #include <iostream>
 #include <fstream>
 
+#include "vec3.h"
+
+#include "ray.h"
+
+
+class Sphere{
+    private:
+        vec3 position;
+        vec3 color ;
+        double radius;
+    
+    public:
+        // Getters
+        vec3 getPosition() const {
+            return position;
+        }
+
+        vec3 getColor() const {
+            return color;
+        }
+
+        double getRadius() const {
+            return radius;
+        }
+
+        // Setters
+        void setPosition(const vec3& newPosition) {
+            position = newPosition;
+        }
+
+        void setColor(const vec3& newColor) {
+            color = newColor;
+        }
+
+        void setRadius(double newRadius) {
+            radius = newRadius;
+        }
+        
+};
+
+
+
 int main()
 {
 }
--- vec3.h
+++ vec3.h
@@ -21,6 +21,7 @@
 
 #include <cmath>
 #include <iostream>
+#include <limits>
 
 // Constants
 
